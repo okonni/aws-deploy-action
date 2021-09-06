4ebbdb2cc36b7bff7836656d15b5849e00bb4423
mircea <mircea.preotu@gmail.com>
2021-09-06 16:03:15 +0200

    updates for hbs-based template recursive buil

--- Dockerfile
+++ Dockerfile
@@ -1,18 +1,20 @@
 FROM node:12
 
-LABEL "com.github.actions.name"="AWS Deploy Action"
-LABEL "com.github.actions.description"="Build distribution using npm, syncs dist folder to an AWS S3 bucket and invalidates a AWS CloudFront distribution."
+LABEL "com.github.actions.name"="Deploy okonni website on AWS"
+LABEL "com.github.actions.description"="Builds static html files from Handlebars templates using npm, syncs dist folder to an AWS S3 bucket, invalidates an AWS CloudFront distribution, deploys Lambda functions using Serverless"
 LABEL "com.github.actions.icon"="upload-cloud"
 LABEL "com.github.actions.color"="green"
 
 LABEL version="1.0.0"
-LABEL repository="https://github.com/mirceapreotu/aws-deploy-action"
-LABEL homepage="https://mirceapreotu.com/"
+LABEL repository="https://github.com/okonni/aws-deploy-action"
+LABEL homepage="https://okonni.com/"
 LABEL maintainer="Mircea Preotu <mircea@incognicode.com>"
 
 RUN apt-get update && apt-get install -y zip && rm -rf /var/lib/apt/lists/*
 RUN curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip" && unzip awscliv2.zip
 RUN ./aws/install && aws --version
+
+RUN npm install -g hbs-cli serverless
 
 ENV PATH /github/workspace/node_modules/.bin:$PATH
 ADD entrypoint.sh /entrypoint.sh
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,26 +1,47 @@
-#!/bin/sh
+#!/bin/bash
 
 set -e
 
+echo "[INFO] Verifying dependencies"
+
 if [ -z "$AWS_ACCESS_KEY_ID" ]; then
-  echo "AWS_ACCESS_KEY_ID is missing"
+  echo "[ERROR] AWS_ACCESS_KEY_ID is missing"
   exit 1
 fi
 
 if [ -z "$AWS_SECRET_ACCESS_KEY" ]; then
-  echo "AWS_SECRET_ACCESS_KEY is missing"
+  echo "[ERROR] AWS_SECRET_ACCESS_KEY is missing"
   exit 1
 fi
 
 if [ -z "$AWS_REGION" ]; then
-  echo "AWS_REGION is missing"
+  echo "[ERROR] AWS_REGION is missing"
   exit 1
 fi
 
 if [ -z "$AWS_S3_BUCKET" ]; then
-  echo "AWS_S3_BUCKET is missing"
+  echo "[ERROR] AWS_S3_BUCKET is missing"
   exit 1
 fi
+
+
+echo "[INFO] Buiding distribution files"
+
+WORKDIR=`pwd`
+
+FILES=`find $WORKDIR | egrep '\.hbs$'`
+
+for file in $FILES; do
+  dest="${file//$WORKDIR/}"
+   dest="${dest//index.hbs/}"
+   dest=$WORKDIR/dist$dest
+  echo "[INFO] > source=$file | target=$dest"
+
+  hbs $file --partial "$WORKDIR/partials/*.hbs" --output $dest
+done 
+
+
+echo "[INFO] Preparing aws deployment environment profile"
 
 aws configure --profile aws-deploy-action <<-EOF > /dev/null 2>&1
 ${AWS_ACCESS_KEY_ID}
@@ -29,22 +50,37 @@ ${AWS_REGION}
 text
 EOF
 
+
+echo "[INFO] Deploying files from dist to S3 ${AWS_S3_BUCKET}/${AWS_S3_BUCKET_FOLDER}"
+
 sh -c "npm install" \
 && sh -c "npm run build" \
 && sh -c "aws s3 sync dist s3://${AWS_S3_BUCKET}/${AWS_S3_BUCKET_FOLDER} \
               --profile aws-deploy-action \
-              --no-progress"
+              --no-progress \
+              --delete"
 SUCCESS=$?
 
 if [ $SUCCESS -eq 0 ]
 then
   if [ -n "$AWS_CLOUDFRONT_DISTRIBUTION_ID" ]; then
+    echo "[INFO] Invalidating Cloudfront distribution (ID=$AWS_CLOUDFRONT_DISTRIBUTION_ID)"
     sh -c "aws cloudfront create-invalidation \
                           --distribution-id ${AWS_CLOUDFRONT_DISTRIBUTION_ID} \
                           --paths /\*"
   fi
 fi
 
+if [ $SUCCESS -eq 0 ]
+then
+  if [ -d "$WORKDIR/lambda" ]; then
+    echo "[INFO] Deploying AWS Lambda functions via Serverless framework"
+    sh -c "cd $WORKDIR/lambda && npm install && SLS_DEBUG=1 serverless deploy"
+  fi
+fi
+
+
+echo "[INFO] Cleaning up aws deployment environment profile"
 aws configure --profile aws-deploy-action <<-EOF > /dev/null 2>&1
 null
 null
@@ -52,10 +88,11 @@ null
 text
 EOF
 
+
 if [ $SUCCESS -eq 0 ]
 then
-  echo "aws-deploy-action successful."
+  echo "[INFO] Deployment completed successfully"
 else
-  echo "aws-deploy-action failed."
+  echo "[INFO] Deployment failed"
   exit 1
 fi
